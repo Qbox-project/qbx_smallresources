5a616971dc5a385bb83ed1546c038efa682d3cec
AJ <66404074+ihyajb@users.noreply.github.com>
2022-09-29 08:45:46 -0400

    pretend i didnt leave this in

--- server/main.lua
+++ server/main.lua
@@ -75,13 +75,11 @@ end)
 
 RegisterNetEvent('baseevents:enteringVehicle', function(...)
     local src = source
-    print('fuck me', src, ...)
     TriggerClientEvent('baseevents:enteringVehicle', src, ...)
 end)
 
 RegisterNetEvent('baseevents:enteredVehicle', function(...)
     local src = source
-    print('fuck me2', src, ...)
     TriggerClientEvent('baseevents:enteredVehicle', src, ...)
 end)
 
