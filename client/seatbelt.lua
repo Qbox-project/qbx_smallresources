eeeca85055c97e6178da54ba4d25708d25e40844
xViperAG <82969741+xViperAG@users.noreply.github.com>
2023-02-18 22:07:45 -0500

    Update seatbelt.lua

--- client/seatbelt.lua
+++ client/seatbelt.lua
@@ -220,30 +220,38 @@ RegisterNetEvent('seatbelt:client:UseHarness', function(ItemData)
     if cache.vehicle and class ~= 8 and class ~= 13 and class ~= 14 then
         if not harnessOn then
             LocalPlayer.state:set('inv_busy', true, true)
-            QBCore.Functions.Progressbar('harness_equip', 'Attaching Race Harness', 5000, false, true, {
-                disableMovement = false,
-                disableCarMovement = false,
-                disableMouse = false,
-                disableCombat = true,
-            }, {}, {}, {}, function()
+            if lib.progressCircle({
+                duration = 5000,
+                label = 'Attaching Race Harness',
+                position = 'bottom',
+                useWhileDead = false,
+                canCancel = true,
+                disable = {
+                    combat = true
+                }
+            }) then
                 LocalPlayer.state:set('inv_busy', false, true)
                 ToggleHarness()
                 TriggerServerEvent('equip:harness', ItemData)
-            end)
-            harnessHp = ItemData.info.uses
+            end
+            harnessHp = ItemData.metadata.harnessuses
             harnessData = ItemData
             TriggerEvent('hud:client:UpdateHarness', harnessHp)
         else
             LocalPlayer.state:set('inv_busy', true, true)
-            QBCore.Functions.Progressbar('harness_equip', 'Removing Race Harness', 5000, false, true, {
-                disableMovement = false,
-                disableCarMovement = false,
-                disableMouse = false,
-                disableCombat = true,
-            }, {}, {}, {}, function()
+            if lib.progressCircle({
+                duration = 5000,
+                label = 'Removing Race Harness',
+                position = 'bottom',
+                useWhileDead = false,
+                canCancel = true,
+                disable = {
+                    combat = true
+                }
+            }) then
                 LocalPlayer.state:set('inv_busy', false, true)
                 ToggleHarness()
-            end)
+            end
         end
     else
         QBCore.Functions.Notify('You\'re not in a car.', 'error')
