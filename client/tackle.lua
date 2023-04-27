f36565e783f43d7c96c4cd8c615274e21638fe52
Gergo <69343477+Rav3n95@users.noreply.github.com>
2023-04-27 12:02:05 +0200

    Utilise lib.addKeybind in tackle.lua

--- client/tackle.lua
+++ client/tackle.lua
@@ -1,22 +1,31 @@
 local QBCore = exports['qbx-core']:GetCoreObject()
-RegisterCommand('tackle', function()
-    if not QBCore.Functions.GetPlayerData().metadata["ishandcuffed"] and (IsPedSprinting(cache.ped) or IsPedRunning(cache.ped)) then
-        local closestPlayer, closestDistance = QBCore.Functions.GetClosestPlayer()
-        if closestDistance ~= -1 and closestDistance < 1.6 then
-            if not cache.vehicle and not IsPedInAnyVehicle(GetPlayerPed(closestPlayer)) then
-                TriggerServerEvent('tackle:server:TacklePlayer', GetPlayerServerId(closestPlayer))
-                lib.requestAnimDict('swimming@first_person@diving')
-                TaskPlayAnim(cache.ped, 'swimming@first_person@diving', 'dive_run_fwd_-45_loop' ,3.0, 3.0, -1, 49, 0, false, false, false)
-                Wait(250)
-                ClearPedTasks(cache.ped)
-                SetPedToRagdoll(cache.ped, 150, 150, 0, 0, 0, 0)
-                RemoveAnimDict('swimming@first_person@diving')
-            end
-        end
-    end
-end, false)
 
-RegisterKeyMapping('tackle', 'Tackle', 'keyboard', 'e')
+lib.addKeybind({
+    name = 'tackle',
+    description = 'Tackle',
+    defaultKey = 'E',
+    onReleased = function(self)
+        if cache.vehicle then return end
+        if QBCore.Functions.GetPlayerData().metadata["ishandcuffed"] then return end
+        if IsPedSprinting(cache.ped) or IsPedRunning(cache.ped) then
+            local coords = GetEntityCoords(cache.ped)
+            local targetId, targetPed, _ = lib.getClosestPlayer(coords, 1.6, false)
+            if not targetPed then return end
+            if IsPedInAnyVehicle(targetPed, true) then return end
+            self:disable(true)
+            TriggerServerEvent('tackle:server:TacklePlayer', GetPlayerServerId(targetId))
+            lib.requestAnimDict('swimming@first_person@diving')
+            TaskPlayAnim(cache.ped, 'swimming@first_person@diving', 'dive_run_fwd_-45_loop' ,3.0, 3.0, -1, 49, 0, false, false, false)
+            Wait(250)
+            ClearPedTasks(cache.ped)
+            SetPedToRagdoll(cache.ped, 150, 150, 0, 0, 0, 0)
+            RemoveAnimDict('swimming@first_person@diving')
+        end
+        SetTimeout(10000, function ()
+            self:disable(false)
+        end)
+    end
+})
 
 RegisterNetEvent('tackle:client:GetTackled', function()
 	SetPedToRagdoll(cache.ped, 7000, 7000, 0, 0, 0, 0)
