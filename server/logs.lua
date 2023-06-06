83f892c80587b605a33793848dcc9668717183c0
Scully <51968381+Scullyy@users.noreply.github.com>
2023-06-06 12:53:40 -0700

    Log queue
    Log queue for discord logs, will prevent being rate limited and on the odd chance you do get rate limited it'll handle those also

--- server/logs.lua
+++ server/logs.lua
@@ -1,4 +1,6 @@
 local QBCore = exports['qbx-core']:GetCoreObject()
+local logQueue, isProcessingQueue, logCount = {}, false, 0
+local lastRequestTime, requestDelay = 0, 0
 
 local Webhooks = {
     ['default'] = '',
@@ -62,6 +64,71 @@ end
 
 exports('OxLog', OxLog)
 
+---Log Queue
+local function applyRequestDelay()
+    local currentTime = GetGameTimer()
+    local timeDiff = currentTime - lastRequestTime
+
+    if timeDiff < requestDelay then
+        local remainingDelay = requestDelay - timeDiff
+
+        Wait(remainingDelay)
+    end
+
+    lastRequestTime = GetGameTimer()
+end
+
+local allowedErr = {
+    [200] = true,
+    [201] = true,
+    [204] = true,
+    [304] = true
+}
+
+---Log Queue
+---@param payload Log Queue
+local function logPayload(payload)
+    PerformHttpRequest(payload.webhook, function(err, text, headers)
+        if err and not allowedErr[err] then
+            print('^1Error occurred while attempting to send log to discord: ' .. err .. '^7')
+            return
+        end
+
+        local remainingRequests = tonumber(headers["X-RateLimit-Remaining"])
+        local resetTime = tonumber(headers["X-RateLimit-Reset"])
+
+        if remainingRequests and resetTime and remainingRequests == 0 then
+            local currentTime = os.time()
+            local resetDelay = resetTime - currentTime
+
+            if resetDelay > 0 then
+                requestDelay = resetDelay * 1000 / 10
+            end
+        end
+    end, 'POST', json.encode({content = payload.tag and '@everyone' or nil, embeds = {payload.embed}}), { ['Content-Type'] = 'application/json' })
+end
+
+---Log Queue
+local function processLogQueue()
+    if #logQueue > 0 then
+        local payload = table.remove(logQueue, 1)
+
+        logPayload(payload)
+
+        logCount += 1
+
+        if logCount % 5 == 0 then
+            Wait(60000)
+        else
+            applyRequestDelay()
+        end
+
+        processLogQueue()
+    else
+        isProcessingQueue = false
+    end
+end
+
 ---Logs to discord regardless of Config.EnableDiscordLogging value
 ---@param name string source of the log. Usually a playerId or name of a script.
 ---@param title string the action or 'event' being logged. Usually a verb describing what the name is doing. Example: SpawnVehicle
@@ -76,7 +143,7 @@ local function DiscordLog(name, title, message, color, tagEveryone)
             ['title'] = title,
             ['color'] = Colors[color] or Colors['default'],
             ['footer'] = {
-                ['text'] = os.date('%c'),
+                ['text'] = os.date('%H:%M:%S %m-%d-%Y'),
             },
             ['description'] = message,
             ['author'] = {
@@ -85,7 +152,18 @@ local function DiscordLog(name, title, message, color, tagEveryone)
             },
         }
     }
-    PerformHttpRequest(webHook, function() end, 'POST', json.encode({ username = 'QB Logs', content = tag and '@everyone' or nil, embeds = embedData }), { ['Content-Type'] = 'application/json' })
+
+    logQueue[#logQueue + 1] = {
+        webhook = webHook,
+        tag = tag,
+        embed = embedData
+    }
+
+    if not isProcessingQueue then
+        isProcessingQueue = true
+
+        CreateThread(processLogQueue)
+    end
 end
 
 exports('DiscordLog', DiscordLog)
