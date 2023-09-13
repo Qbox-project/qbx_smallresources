7085cbc223d51efe90413e0bf147de5a4095833a
FjamZoo <34508112+FjamZoo@users.noreply.github.com>
2023-09-13 10:26:53 +0200

    feat(entityCreating): Only load if clients can spawn networked entities

--- server/entities.lua
+++ server/entities.lua
@@ -1,3 +1,7 @@
+local bucketLockDownMode = GetConvar('qbx:bucketlockdownmode', 'relaxed')
+
+if bucketLockDownMode ~= 'inactive' then return end
+
 -- Blacklisting entities can just be handled entirely server side with onesync events
 -- No need to run coroutines to supress or delete these when we can simply delete them before they spawn
 AddEventHandler('entityCreating', function(handle)
