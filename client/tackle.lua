550676896141c7583ebc2b76aecd72e545424275
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2023-09-26 17:02:21 -0400

    Update tackle.lua
    Moved:
    
     SetTimeout(1000, function ()
                    self:disable(false) 
    
    to make it stop the tackle function. When outside it was running multiple threads as many times as the tackle key was pressed.

--- client/tackle.lua
+++ client/tackle.lua
@@ -4,27 +4,27 @@ lib.addKeybind({
     defaultKey = 'E',
     onReleased = function(self)
         if cache.vehicle then return end
-        if QBX.PlayerData.metadata.ishandcuffed then return end
-        if IsPedSprinting(cache.ped) or IsPedRunning(cache.ped) then
-            local coords = GetEntityCoords(cache.ped)
+        if QBXore.Functions.GetPlayerData().metadata["ishandcuffed"] and not IsPedRagdoll(PlayerPedId()) then return end   
+        if IsPedSprinting(PlayerPedId()) or IsPedRunning(PlayerPedId()) then
+            local coords = GetEntityCoords(PlayerPedId())
             local targetId, targetPed, _ = lib.getClosestPlayer(coords, 1.6, false)
             if not targetPed then return end
             if IsPedInAnyVehicle(targetPed, true) then return end
             self:disable(true)
             TriggerServerEvent('tackle:server:TacklePlayer', GetPlayerServerId(targetId))
             lib.requestAnimDict('swimming@first_person@diving')
-            TaskPlayAnim(cache.ped, 'swimming@first_person@diving', 'dive_run_fwd_-45_loop', 3.0, 3.0, -1, 49, 0, false, false, false)
+            TaskPlayAnim(PlayerPedId(), 'swimming@first_person@diving', 'dive_run_fwd_-45_loop', 3.0, 3.0, -1, 49, 0, false, false, false)
             Wait(250)
-            ClearPedTasks(cache.ped)
-            SetPedToRagdoll(cache.ped, 150, 150, 0, 0, 0, 0)
+            ClearPedTasks(PlayerPedId())
+            SetPedToRagdoll(PlayerPedId(), 150, 150, 0, 0, 0, 0)
             RemoveAnimDict('swimming@first_person@diving')
+            SetTimeout(1000, function ()
+                self:disable(false)
+            end)
         end
-        SetTimeout(10000, function ()
-            self:disable(false)
-        end)
     end
 })
 
 RegisterNetEvent('tackle:client:GetTackled', function()
-	SetPedToRagdoll(cache.ped, 7000, 7000, 0, 0, 0, 0)
+	SetPedToRagdoll(PlayerPedId(), 7000, 7000, 0, 0, 0, 0)
 end)
