e6b2f7504eae5021c2a9eaa5740cd4a2a737cd73
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2023-09-26 17:06:06 -0400

    Update tackle.lua

--- client/tackle.lua
+++ client/tackle.lua
@@ -4,7 +4,7 @@ lib.addKeybind({
     defaultKey = 'E',
     onReleased = function(self)
         if cache.vehicle then return end
- if QBX.PlayerData.metadata.ishandcuffed then return end
+ 	if QBX.PlayerData.metadata.ishandcuffed then return end
         if IsPedSprinting(cache.ped) or IsPedRunning(cache.ped) then
             local coords = GetEntityCoords(cache.ped)
             local targetId, targetPed, _ = lib.getClosestPlayer(coords, 1.6, false)
@@ -13,10 +13,10 @@ lib.addKeybind({
             self:disable(true)
             TriggerServerEvent('tackle:server:TacklePlayer', GetPlayerServerId(targetId))
             lib.requestAnimDict('swimming@first_person@diving')
-            TaskPlayAnim(PlayerPedId(), 'swimming@first_person@diving', 'dive_run_fwd_-45_loop', 3.0, 3.0, -1, 49, 0, false, false, false)
+            TaskPlayAnim(cache.ped, 'swimming@first_person@diving', 'dive_run_fwd_-45_loop', 3.0, 3.0, -1, 49, 0, false, false, false)
             Wait(250)
-            ClearPedTasks(PlayerPedId())
-            SetPedToRagdoll(PlayerPedId(), 150, 150, 0, 0, 0, 0)
+            ClearPedTasks(cache.ped)
+            SetPedToRagdoll(cache.ped, 150, 150, 0, 0, 0, 0)
             RemoveAnimDict('swimming@first_person@diving')
             SetTimeout(1000, function ()
                 self:disable(false)
@@ -26,5 +26,5 @@ lib.addKeybind({
 })
 
 RegisterNetEvent('tackle:client:GetTackled', function()
-	SetPedToRagdoll(PlayerPedId(), 7000, 7000, 0, 0, 0, 0)
+	SetPedToRagdoll(cache.ped, 7000, 7000, 0, 0, 0, 0)
 end)
