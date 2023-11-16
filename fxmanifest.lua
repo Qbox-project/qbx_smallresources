5a9b0ee3cd93a35aebb3f95b1220323a8a503f2c
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 10:12:03 -0500

    fix: import modules
    * fix: import modules
    
    * fix: missing letter

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,12 +7,16 @@ version '1.1.0'
 
 shared_scripts {
 	'@ox_lib/init.lua',
-	'@qbx_core/import.lua',
+	'@qbx_core/modules/utils.lua',
 	'config.lua'
 }
 
+client_scripts {
+	'@qbx_core/modules/playerdata.lua',
+	'client/*.lua'
+}
+
 server_script 'server/*.lua'
-client_script 'client/*.lua'
 
 data_file 'FIVEM_LOVES_YOU_4B38E96CC036038F' 'events.meta'
 data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'popgroups.ymt'
@@ -21,11 +25,6 @@ files {
 	'events.meta',
 	'popgroups.ymt',
 	'relationships.dat'
-}
-
-modules {
-	'qbx_core:playerdata',
-	'qbx_core:utils'
 }
 
 lua54 'yes'
