e4954cde397807fdb73dd554c758e2efaf0d6676
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-02-12 03:19:00 +0100

    refactor: improve consumeables logic even more

--- config.lua
+++ config.lua
@@ -31,23 +31,141 @@ Config.Density = {
     ['scenario'] = 0.8,
 }
 
+---@meta
+---@class anim
+---@field clip string
+---@field dict string
+---@field flag number
+
+---@meta
+---@class prop
+---@field model string
+---@field bone number
+---@field pos vector3
+---@field rot vector3
+
+---@meta
+---@class stressRelief
+---@field min number
+---@field max number
+
+---@meta
+---@class consumeables
+---@field min number
+---@field max number
+---@field anim anim?
+---@field prop table?
+---@field stressRelief table?
+
+---@class ConsumablesEat : consumeables
+
+---@type ConsumablesEat
 ConsumablesEat = {
-    ['sandwich'] = math.random(35, 54),
-    ['tosti'] = math.random(40, 50),
-    ['twerks_candy'] = math.random(35, 54),
-    ['snikkel_candy'] = math.random(40, 50),
+    ['sandwich'] = {
+        min = 35,
+        max = 54,
+        stressRelief = {
+            min = 1,
+            max = 4
+        },
+    },
+    ['tosti'] = {
+        min = 40,
+        max = 50,
+        stressRelief = {
+            min = 1,
+            max = 4
+        },
+    },
+    ['twerks_candy'] = {
+        min = 35,
+        max = 54,
+        stressRelief = {
+            min = 1,
+            max = 4
+        },
+    },
+    ['snikkel_candy'] = {
+        min = 40,
+        max = 50,
+        stressRelief = {
+            min = 1,
+            max = 4
+        },
+    },
 }
 
+---@class ConsumablesDrink : consumeables
+
+---@type ConsumablesDrink
 ConsumablesDrink = {
-    ['water_bottle'] = math.random(35, 54),
-    ['kurkakola'] = math.random(35, 54),
-    ['coffee'] = math.random(40, 50),
+    ['water_bottle'] = {
+        min = 35,
+        max = 54,
+        stressRelief = {
+            min = 1,
+            max = 4
+        },
+    },
+    ['kurkakola'] = {
+        min = 35,
+        max = 54,
+        stressRelief = {
+            min = 1,
+            max = 4
+        },
+    },
+    ['coffee'] = {
+        min = 40,
+        max = 50,
+        anim = {
+            clip = 'idle_c',
+            dict = 'amb@world_human_drinking@coffee@male@idle_a',
+            flag = 49
+        },
+        prop = {
+            model = 'p_amb_coffeecup_01',
+            bone = 28422,
+            pos = {x = 0.0, y = 0.0, z = 0.0},
+            rot = {x = 0.0, y = 0.0, z = 0.0}
+        },
+        stressRelief = {
+            min = -10,
+            max = -1
+        },
+    },
 }
 
+---@class ConsumablesAlcohol : consumeables
+    ---@field alcoholLevel? number
+
+---@type ConsumablesAlcohol
 ConsumablesAlcohol = {
-    ['whiskey'] = math.random(20, 30),
-    ['beer'] = math.random(30, 40),
-    ['vodka'] = math.random(20, 40),
+    ['whiskey'] = {
+        min = 20,
+        max = 30,
+        stressRelief = {
+            min = 1,
+            max = 4
+        },
+    },
+    ['beer'] = {
+        min = 30,
+        max = 40,
+        stressRelief = {
+            min = 1,
+            max = 4
+        },
+        alcoholLevel = 0.25
+    },
+    ['vodka'] = {
+        min = 20,
+        max = 40,
+        stressRelief = {
+            min = 1,
+            max = 4
+        },
+    },
 }
 
 Config.BlacklistedScenarios = {
