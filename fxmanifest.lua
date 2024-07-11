e183c8f42ce2a2667cedfef0e1263f1d9f82428e
solareon <solareon@users.noreply.github.com>
2024-07-11 18:30:17 +0000

    chore: bump manifest version to v0.1.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'qbx_smallresources'
 description 'Collection of small scripts'
 repository 'https://github.com/Qbox-project/qbx_smallresources'
-version '0.1.0'
+version '0.1.1'
 
 ox_lib 'locale'
 
