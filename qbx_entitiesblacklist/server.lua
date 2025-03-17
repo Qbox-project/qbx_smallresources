51ae5c6d94ce0667361a498784c5072b339f851d
Snowly <visionhyronic@gmail.com>
2025-03-18 00:22:32 +0100

    feat(qbx_smallresources): Add Config check and change from qbx_smallresources to qbx_entitiesblacklist

--- qbx_entitiesblacklist/server.lua
+++ qbx_entitiesblacklist/server.lua
@@ -1,11 +1,16 @@
 local config = require 'qbx_entitiesblacklist.config'
 -- Blacklisting entities can just be handled entirely server side with onesync events
 -- No need to run coroutines to supress or delete these when we can simply delete them before they spawn
+if type(config.blacklisted) ~= 'table' then
+    lib.print.warning('[qbx_entitiesblacklist] Blacklisted vehicles is not a table')
+    return
+end
+
 AddEventHandler('entityCreating', function(handle)
     local entityModel = GetEntityModel(handle)
 
     if config.blacklisted[entityModel] then
-        lib.print.warning('[qbx_smallresources] Blocked entity creation for model: ' .. entityModel)
+        lib.print.warning('[qbx_entitiesblacklist] Blocked entity creation for model: ' .. entityModel)
         CancelEvent()
     end
 end)
