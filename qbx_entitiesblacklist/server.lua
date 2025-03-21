94c78fd1e2297c9875a06672db8faf22b4b2783f
Snowly <visionhyronic@gmail.com>
2025-03-21 11:12:53 +0100

    Change to 1 time warning instead of each time an entity is created.

--- qbx_entitiesblacklist/server.lua
+++ qbx_entitiesblacklist/server.lua
@@ -1,4 +1,5 @@
 local config = require 'qbx_entitiesblacklist.config'
+lib.print.warn('[qbx_entitiesblacklist] You have entity blacklist enabled, this means that any entity that is blacklisted in config.lua will not be able to spawn, you can change this in qbx_smallresource/qbx_entitiesblacklist/config.lua')
 -- Blacklisting entities can just be handled entirely server side with onesync events
 -- No need to run coroutines to supress or delete these when we can simply delete them before they spawn
 if type(config.blacklisted) ~= 'table' then
@@ -10,7 +11,6 @@ AddEventHandler('entityCreating', function(handle)
     local entityModel = GetEntityModel(handle)
 
     if config.blacklisted[entityModel] then
-        lib.print.warning('[qbx_entitiesblacklist] Blocked entity creation for model: ' .. entityModel)
         CancelEvent()
     end
 end)
