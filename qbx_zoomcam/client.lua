b51115075709985ebce784e8dca89742e08a11bc
Yuga Bharathi Jaisankar <yuga.bharathijai2106@gmail.com>
2026-06-27 15:43:19 +0530

    feat: implement custom zoom camera functionality with smooth FOV and offset interpolation

--- qbx_zoomcam/client.lua
+++ qbx_zoomcam/client.lua
@@ -1,6 +1,7 @@
 local Config = require 'qbx_zoomcam.config'
 
 local currentFOV = Config.DefaultFOV
+local currentZOffset = 0.0
 local zoomCam = nil
 local isHolding = false
 
@@ -9,9 +10,15 @@ local function ZoomLoop()
 
     CreateThread(function()
         -- Create cam on first hold
+        currentFOV = GetGameplayCamFov()
         zoomCam = CreateCam("DEFAULT_SCRIPTED_CAMERA", true)
         local camCoords = GetGameplayCamCoord()
+        local camRot = GetGameplayCamRot(2)
+        
         SetCamCoord(zoomCam, camCoords.x, camCoords.y, camCoords.z)
+        SetCamRot(zoomCam, camRot.x, camRot.y, camRot.z, 2)
+        SetCamFov(zoomCam, currentFOV)
+        
         RenderScriptCams(true, false, 0, true, false)
 
         -- Keep looping while holding OR while the FOV is still transitioning back to default
@@ -20,22 +27,25 @@ local function ZoomLoop()
             
             -- Set target based on hold state
             local targetFOV = isHolding and Config.ZoomFOV or Config.DefaultFOV
+            local targetZOffset = isHolding and 0.3 or 0.0
             local speed = isHolding and Config.ZoomInSpeed or Config.ZoomOutSpeed
             
-            -- Lerp FOV smoothly
+            -- Lerp FOV and Z Offset smoothly
             currentFOV = currentFOV + (targetFOV - currentFOV) / speed
+            currentZOffset = currentZOffset + (targetZOffset - currentZOffset) / speed
 
             -- Update cam every frame
             local coords = GetGameplayCamCoord()
             local rot = GetGameplayCamRot(2)
 
-            SetCamCoord(zoomCam, coords.x, coords.y, coords.z + 0.3)
+            SetCamCoord(zoomCam, coords.x, coords.y, coords.z + currentZOffset)
             SetCamRot(zoomCam, rot.x, rot.y, rot.z, 2)
             SetCamFov(zoomCam, currentFOV)
         end
 
         -- Destroy cam when fully back to default and not holding
         currentFOV = Config.DefaultFOV
+        currentZOffset = 0.0
         RenderScriptCams(false, false, 0, true, false)
         DestroyCam(zoomCam, false)
         zoomCam = nil
